dd1c1f7dffcdc84915747106ede731fa3ca46c2a
Nilo Flora Jr <52353875+nilo0522@users.noreply.github.com>
2021-08-12 13:16:41 +0800

    Update Controller.php

--- src/Http/Controllers/Controller.php
+++ src/Http/Controllers/Controller.php
@@ -6,9 +6,9 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-use Fligno\PaginatorTrait\Paginators;
+//use Fligno\PaginatorTrait\Paginators;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests, Paginators;
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;//, Paginators;
 }
